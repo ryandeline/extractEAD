95230c35dda91d7007263e104050d67ec1ab1213
Ryan DeLine <ryan.deline@gmail.com>
2015-06-24 21:10:05 -0400

    write single cvs

--- extractEAD.py
+++ extractEAD.py
@@ -1,25 +1,41 @@
+import csv ,os
 from lxml import etree
 import xml.etree.ElementTree as et
-import os
 
 parser = etree.XMLParser(remove_comments=False)
-
-inpath = '/Users/admin/Documents/pyEAD/in'
-outpath = '/Users/admin/Documents/pyEAD/out'
+inpath = '/Users/admin/Documents/extractEAD/in'
+outpath = '/Users/admin/Documents/extractEAD/out'
 
 for filename in os.listdir(inpath):
 	if not filename.endswith('.xml'): continue
 	fullname = os.path.join(inpath, filename)
 	tree = etree.parse(fullname)
 	root = tree.getroot()
-	# root.append(etree.Comment('Microsoft Photogrammetry'))
-	for image_number in root.iter('image_number'):
-		new_name = int(image_number.text) + 3000
-		image_number.text = str(new_name)
-	for exposure_number in root.iter('exposure_number'):
-		new_name = int(exposure_number.text) + 3000
-		exposure_number.text = str(new_name)
 
-	outname = os.path.join(outpath, filename)
-	tree.write(outname,xml_declaration=True, encoding="utf-8",)
+	NS = "urn:schemas-microsoft-com:datatypes"
+	header = ('Exposure', 'Latitude', 'Longitude', 'Altitude', 'Date', 'Time')
+	
+	with open('output.csv', 'w') as f:
+		writer = csv.writer(f)
+		writer.writerow(header)
+		for exposure_number in root.iter('exposure_number'):
+			exp = int(exposure_number.text)
+			exposure_number.text = str(exp)
+			# exposure_number = exposure_number.get('exposure_number')
+		for latitude in root.iter('latitude'):
+			latitude = latitude.get('latitude')
+			
+			row = exposure_number, latitude
+			writer.writerow(row)
+
+
+			# Write XML
+			# outname = os.path.join(outpath, filename)
+			# tree.write(outname,xml_declaration=True, encoding="utf-8",)
+			
+			
+			
+
+
+
 	# print(len(parser.error_log))
